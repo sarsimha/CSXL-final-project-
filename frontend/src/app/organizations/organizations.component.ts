62e646df590087f513e10833ce723951e3ce0802
Sonika Puvvada <69735739+sonikacp@users.noreply.github.com>
2023-03-30 21:14:06 -0400

    Create table for viewing student organizations and descriptions. Fix issues with observable array type. Finish connecting backend to frontend.

--- frontend/src/app/organizations/organizations.component.ts
+++ frontend/src/app/organizations/organizations.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
+import { Observable } from 'rxjs';
 import { isAuthenticated } from 'src/app/gate/gate.guard';
+import { Organization, OrganizationsService } from './organizations.service';
 
 
 
@@ -9,20 +11,17 @@ import { isAuthenticated } from 'src/app/gate/gate.guard';
   templateUrl: './organizations.component.html',
   styleUrls: ['./organizations.component.css']
 })
-export class OrganizationsComponent implements OnInit {
+export class OrganizationsComponent {
   public static Route: Route = {
     path: 'organizations',
     component: OrganizationsComponent,
     title: 'Organizations', 
     canActivate: [isAuthenticated], 
-    // canActivate: [isAuthenticated]
   }
+  public allOrganizations$: Observable<Organization[]>
 
-  constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute, private organizationsService: OrganizationsService) {
+    this.allOrganizations$ = organizationsService.getAllOrganizations()
 
-  }
-
-  ngOnInit(): void {
-    
   }
 }
