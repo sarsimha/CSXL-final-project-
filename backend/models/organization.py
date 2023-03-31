6ad240b62203f407f29057e97e46709ce89ef28b
Kelly Fan <kfan@email.unc.edu>
2023-03-31 14:18:08 -0400

    Fixed nit and removed link from data model

--- backend/models/organization.py
+++ backend/models/organization.py
@@ -6,7 +6,5 @@ class Organization(BaseModel):
     id: int | None = None
     name: str
     description: str
-    
-    link: str
     # TODO: Add events: list[Events] and create Event model
     # TODO: Figure out how to connect organization and events through ID
