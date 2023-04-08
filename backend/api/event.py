b53a2a72e63b5ff643d3e6af14d64f48682acbab
Kelly Fan <kfan@email.unc.edu>
2023-04-08 18:39:04 -0400

    Change event variable name

--- backend/api/event.py
+++ backend/api/event.py
@@ -6,18 +6,20 @@ from ..models import Event, CreateEvent
 api = APIRouter(prefix="/api/event")
 
 
+# View all events in database
 @api.get("", response_model=list[Event], tags=['Event'])
 def get_events(eventService: EventService = Depends()) -> list[Event]:
     return eventService.all()
 
+# Create new event
 @api.post("/create", response_model=Event, tags=['Event'])
 async def create_event(
     create: CreateEvent,
     eventService: EventService = Depends()
 ):
     #TODO: check if event already exists or not
-    event = Event(
-        #TODO: figure out how ID is added or not
+    # Use form data to create new event
+    newEvent = Event(
         name=create.name,
         orgName=create.orgName,
         location=create.location,
@@ -25,5 +27,6 @@ async def create_event(
         date=create.date,
         time=create.time
     )
-    event = eventService.create_event(event)
-    return event
+    # Add new event to database
+    newEvent = eventService.create_event(newEvent)
+    return newEvent
