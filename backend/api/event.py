fe102008981831e1a504ce0e60c582a881eb4e6b
Kelly Fan <kfan@email.unc.edu>
2023-04-08 17:33:23 -0400

    Create events form backend API and services

--- backend/api/event.py
+++ backend/api/event.py
@@ -1,9 +1,29 @@
-from fastapi import APIRouter, Depends
+# from typing import Annotated
+from fastapi import APIRouter, Depends #, Form
 from ..services import EventService
-from ..models import Event
-
+from ..models import Event, CreateEvent
 
 api = APIRouter(prefix="/api/event")
+
+
 @api.get("", response_model=list[Event], tags=['Event'])
 def get_events(eventService: EventService = Depends()) -> list[Event]:
     return eventService.all()
+
+@api.post("/create", response_model=Event, tags=['Event'])
+async def create_event(
+    create: CreateEvent,
+    eventService: EventService = Depends()
+):
+    #TODO: check if event already exists or not
+    event = Event(
+        #TODO: figure out how ID is added or not
+        name=create.name,
+        orgName=create.orgName,
+        location=create.location,
+        description=create.description,
+        date=create.date,
+        time=create.time
+    )
+    event = eventService.create_event(event)
+    return event
