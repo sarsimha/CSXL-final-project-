59f4354f0f20d291af492a829e1daeb89071ad10
Kelly Fan <69778740+krenfan@users.noreply.github.com>
2023-04-24 13:38:36 -0400

    Eli Exec: Complete frontend for delete event functionality (#79)
    * Complete frontend for delete event functionality
    
    * Address nit

--- frontend/src/app/event/event.component.ts
+++ frontend/src/app/event/event.component.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { isAuthenticated } from 'src/app/gate/gate.guard';
 import { Event, EventService } from './event.service';
 import { Organization, OrganizationsService } from '../organizations/organizations.service';
+import { PermissionService } from '../permission.service';
 import { FormControl } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 
@@ -29,10 +30,23 @@ export class EventComponent {
   public organizations$: Observable<Organization[]>;
   public org: any;
   public orgControl = new FormControl('');
+  public execPermission$: Observable<boolean>;
 
-  constructor(route: ActivatedRoute, private eventService: EventService, private orgService: OrganizationsService) {
+  constructor(
+    route: ActivatedRoute, 
+    private eventService: EventService, 
+    private orgService: OrganizationsService,
+    private permission: PermissionService
+    ) {
     this.allEvents$ = eventService.getAllEvents()
     this.organizations$ = orgService.getAllOrganizations()
+    this.execPermission$ = this.permission.check('event.delete_event', 'event/delete/')
+
+    this.execPermission$.subscribe(permission => {
+      if (permission) {
+        this.displayedColumns.push('delete')
+      }
+    });
   }
 
   public searchOrganizations(org: string) {
@@ -47,5 +61,15 @@ export class EventComponent {
       .subscribe(data => {
         this.allEvents$ = of(data);
       });
+  }
+
+  public deleteEvent(eventId: number) {
+    //add pop-up check to confirm event deletion
+    this.eventService.deleteEvent(eventId)
+      .subscribe(() => {
+        this.allEvents$ = this.eventService.getAllEvents();
+      });
+    //reload so user doesn't face "500 internal service error" pop-up
+    window.location.reload()
   }
 }
